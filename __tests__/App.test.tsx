c8a24adbf9de9a548736d7d2a61564f658fde2e2
Gayathri Perumal <gayathriperumal100@gmail.com>
2024-05-03 20:23:13 +0530

    Create first snapshot

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -7,11 +7,16 @@ import React from 'react';
 import App from '../App';
 
 // Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
+import {it, test, expect} from '@jest/globals';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
+});
+
+test('should match the tree', () => {
+  const tree = renderer.create(<App />).toJSON();
+  expect(tree).toMatchSnapshot();
 });
